a90fb6d7550e9e9ca3196885dc56c312786f54f0
zobayertihan <zobayertihan@gmail.com>
2022-12-14 23:52:12 +0600

    Contacts added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import About from './About';
 import './App.css';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Projects from './Components/Projects';
+import Resume from './Components/Resume';
+import Skills from './Components/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen flex flex-col'>
+      <Header></Header>
+      <div className='w-max-[1000px] mx-auto flex flex-col gap-16'>
+        <Resume></Resume>
+        <About></About>
+        <Skills></Skills>
+        <Projects></Projects>
+        <Contact></Contact>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1 className='text-5xl border-b-2 border-violet-400'>Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
