15c095cebc44fb260a20d7437607ba9c713020e0
zobayertihan <zobayertihan@gmail.com>
2022-12-14 23:54:36 +0600

    Resume added

---
+++ src/Components/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div className=' max-w-[1000px] mx-auto px-10 flex flex-col'>
+            <p>Hi, My name is</p>
+            <h1 className=' text-7xl'>Abu Zobayer Bin Siddique</h1>
+            <h3 className='text-7xl'>I'm a Full Stack Developer</h3>
+            <p className='text-wrap  text-lg'>As a full-stack developer, my goal is to produce exceptional digital experiences. Right now, my main focus is on building responsive full-stack online applications.</p>
+            <div>
+                <a href="/">Download</a>
+            </div>
+        </div>
+    );
+};
+
+export default Resume;
