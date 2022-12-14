99669d0faba81523f398003f46e8f8a6a6d8c18a
zobayertihan <zobayertihan@gmail.com>
2022-12-14 23:52:57 +0600

    Footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="px-4 mt-12 divide-y bg-[#2B3467]	">
+                <div className="py-6 text-sm text-center ">© 2022 Abu Zobayer Bin Siddique. All rights reserved.</div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
