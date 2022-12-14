6dd4eb6b30119601cac5ccd3f654b3fc67e91c0b
zobayertihan <zobayertihan@gmail.com>
2022-12-14 23:55:19 +0600

    about added

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='max-w-[1000px] mx-auto'>
+            <h1 className='text-5xl border-b-2 border-violet-400'>About</h1>
+            <div className='flex gap-10'>
+                <h1>Hi, I'm Abu Zobayer Bin Siddique, nice to meet you. Please take a look around</h1>
+                <p>I aim to succeed as a developer by utilizing my abilities to make a significant contribution and to scale up my knowledge and expertise. I'm a keen learner of programming and hope to advance my abilities in my quest to become a full stack developer. I think my abilities, together with my diligence and deep interest in technology, would be advantageous to your very prospective of interest.</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
