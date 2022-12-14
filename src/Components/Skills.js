85353157933181ebd2e75be7974fbb7840431b63
zobayertihan <zobayertihan@gmail.com>
2022-12-14 23:54:56 +0600

    Skills added

---
+++ src/Components/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div>
+            <h1 className='text-5xl border-b-2 border-violet-400'>Skills</h1>
+        </div>
+    );
+};
+
+export default Skills;
